b3d54a0a552859b2bc65b0cacd4fa3eefecf0469
Micha Schoone <mc.schoone@avans.nl>
2023-03-21 11:44:50 +0100

    error message styles

--- script.js
+++ script.js
@@ -61,7 +61,14 @@ filterBtns.forEach((button) => {
 function displayData() {
   // check for empty search result
   if (filteredSearch.length < 1) {
-    output.innerHTML = `<p class="result-error">no results</p>`;
+    output.innerHTML = `
+    <div class="result-error">
+      <div class="result-error-inner">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#c6002a" d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
+        <h2>We couldn't find any matches</h2>
+        <p>Double check your search for any typos or spelling errors - or try a different search term</p>
+      </div>
+    </div>`;
     return;
   }
   // Create new component container & map full data set
